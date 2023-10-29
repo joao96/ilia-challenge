b41797a4c8c27caabe642b8042b704cda873521e
João Victor <jvpoletti@gmail.com>
2023-10-29 04:29:15 -0300

    📝 chore(server): add swagger yaml

--- apps/ilia-server/src/main.ts
+++ apps/ilia-server/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
+
+  const yamlFile = fs.readFileSync('./api.yaml', 'utf8');
+  const swaggerDocument = yaml.load(yamlFile);
+
+  SwaggerModule.setup('/api', app, swaggerDocument);
 
   await app.listen(3000);
 }
