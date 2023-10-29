11d737cc1c00ccefe401d3917035e773de45d2b8
João Victor <jvpoletti@gmail.com>
2023-10-29 14:20:13 -0300

    🎨 feat(web): add initial pages

---
+++ apps/web/src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from 'next/app';
+import { wrapper } from '../redux/store';
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../styles/global.styles';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#111",
+    secondary: '#0070f3',
+  },
+}
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default wrapper.withRedux(App);
---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,62 @@
+import { Login } from '../components/login';
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Aside>
+        <strong>Welcome to FashionHub</strong>
+        <p>FashionHub is your one-stop online destination for all things fashion</p>
+      </Aside>
+      <Login />
+    </HomeContainer>
+  )
+}
+
+export const HomeContainer = styled.div`
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+`;
+
+const Aside = styled.aside`
+  flex: 7;
+
+  background: #44c8f5;
+  color: #fff;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  padding: 120px 80px;
+
+  strong {
+    font: 700 3.6rem 'Poppins', sans-serif;
+    line-height: 4.2rem;
+    margin-top: 16px;
+  }
+
+  p {
+    font-size: 2.4rem;
+    line-height: 3.2rem;
+    margin-top: 16px;
+    color: #f8f8f8;
+  }
+
+  @media(max-width: 900px) {
+    flex: 4;
+    padding: 40px 40px;
+    strong {
+      font-size: 3rem;
+    }
+
+    p {
+      font-size: 2rem;
+    }
+  }
+
+  @media(max-width: 720px) {
+    display: none;
+  }
+`;
