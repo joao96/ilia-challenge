d6df5f0317d310620a3cbfc516c0872eca89c915
João Victor <jvpoletti@gmail.com>
2023-10-29 22:59:51 -0300

    🔥 refactor(web): change folder name

--- apps/web/src/components/card-list/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ListContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-  list-style: none;
-`;
